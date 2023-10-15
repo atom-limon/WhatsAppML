005f62cd04d17d454e7fa4a089d831c5b8cfda4f
atom-limon <saburlimon513@gmail.com>
2023-10-15 18:43:50 +0600

    1. successfull account creation 2. dublicate username check 3. dublicate email check

--- app/src/main/java/com/example/whatsappml/MainActivity.kt
+++ app/src/main/java/com/example/whatsappml/MainActivity.kt
@@ -17,66 +17,81 @@ import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.text.trimmedLength
 import androidx.fragment.app.FragmentManager
+import com.example.whatsappml.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import org.w3c.dom.Text
 import kotlin.math.sign
 import kotlin.time.Duration
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var username:EditText
-    private lateinit var email:EditText
-    private lateinit var password:EditText
-    private lateinit var signup:Button
-
+    private lateinit var binding: ActivityMainBinding   //view binder
+    private lateinit var db:FirebaseDatabase
     private lateinit var dbref:DatabaseReference
     private lateinit var dbauth:FirebaseAuth
 
+    private fun nullcheck(name:String, email:String, pass:String): Boolean {
+        if(TextUtils.isEmpty(name)||TextUtils.isEmpty(email)||TextUtils.isEmpty(pass)) return true
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tvalready:TextView = findViewById(R.id.tv_already)
-        tvalready.setOnClickListener{
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //sign up to sign in change activity
+        binding.tvAlready.setOnClickListener{
             var intent:Intent = Intent(this,SignInActivity::class.java)
             startActivity(intent)
         }
-        username = findViewById(R.id.et_username)
-        email = findViewById(R.id.et_email)
-        password = findViewById(R.id.et_password)
-        signup = findViewById(R.id.btn_signup)
 
-        dbref = FirebaseDatabase.getInstance().getReference()
+        db = FirebaseDatabase.getInstance()
+        dbref = db.getReference().child("userinfo")
         dbauth = FirebaseAuth.getInstance()
 
-        signup.setOnClickListener{
-            var txtusername:String = username.text.toString()
-            var txtemail:String = email.text.toString()
-            var txtpassword:String = password.text.toString()
-
-            if(TextUtils.isEmpty(txtusername)||TextUtils.isEmpty(txtemail)
-                || TextUtils.isEmpty(txtpassword))
-                Toast.makeText(this,"Empty Credentials",Toast.LENGTH_SHORT).show()
-            else if(txtpassword.trimmedLength()<8)
-                Toast.makeText(this,"password is too short",Toast.LENGTH_SHORT).show()
-            else
-                registerUser(txtusername,txtemail,txtpassword)
-
-        }
-
-    }
-    private fun registerUser(username:String, email:String, password:String){
-        dbauth.createUserWithEmailAndPassword(email,password).addOnSuccessListener{
-            var map = HashMap<String,String>()
-            map.put("name",username)
-            map.put("email",email)
-            map.put("id", dbauth.currentUser!!.uid)
-            dbref.child("User").child(dbauth.currentUser!!.uid).setValue(map).addOnCompleteListener{
-                if (it.isSuccessful){
-                    Toast.makeText(this,"user created Successfully",Toast.LENGTH_LONG).show()
-                }
+        binding.btnSignup.setOnClickListener{
+            var usrname:String = binding.etUsername.text.toString()
+            var usremail:String = binding.etEmail.text.toString()
+            var usrpass:String = binding.etEmail.text.toString()
+            //check for empty credentials
+            if(nullcheck(usrname,usremail,usrpass)){
+                Toast.makeText(applicationContext, "Empty field's are not allowed", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //check for username validity
+                dbref.orderByChild("username").equalTo(usrname).addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        //if username alreday taken then
+                        if(snapshot.exists()){
+                            Toast.makeText(applicationContext, "username taken", Toast.LENGTH_SHORT).show()
+                        }
+                        // if not taken
+                        else{
+                            dbauth.createUserWithEmailAndPassword(usremail,usrpass).addOnCompleteListener{task->
+                                if(task.isSuccessful){
+                                    //if user created successfully
+                                    dbref.child(dbauth.currentUser!!.uid).child("username").setValue(usrname)
+                                    Toast.makeText(applicationContext, "Sign Up Successfull", Toast.LENGTH_SHORT).show()
+                                }
+                                else{
+                                    //if fails
+                                    Toast.makeText(applicationContext, "Email Exists \n Sign In", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                    }
+                    override fun onCancelled(error: DatabaseError) { TODO("Not yet implemented") }
+                })
             }
         }
+
+
+
     }
 }
 
